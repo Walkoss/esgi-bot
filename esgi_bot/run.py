a750c400acf583351ad81ff4923ab0f417e4723f
Walkoss <Walkoss@users.noreply.github.com>
2018-07-21 18:48:49 +0200

    Started authentication feature

--- esgi_bot/run.py
+++ esgi_bot/run.py
@@ -2,15 +2,27 @@ from esgi_bot import logger
 from esgi_bot.conf import DISCORD_TOKEN
 from discord.ext import commands
 
+COMMANDS = [
+    'absence',
+    'mark',
+    'teacher',
+    'login'
+]
+
 
 def run():
     bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
 
-    for extension in ['notes']:
+    for extension in ['commands.' + command for command in COMMANDS]:
         try:
             bot.load_extension(extension)
         except Exception as e:
             logger.error('{}: {}'.format(type(e).__name__, e))
+
+    @bot.event
+    async def on_command_error(error, ctx):
+        if isinstance(error, commands.errors.CheckFailure):
+            await bot.send_message(ctx.message.author, "You're not authenticated")
 
     bot.run(DISCORD_TOKEN)
 
