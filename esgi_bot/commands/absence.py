16c3635286c156543e148c3ab95444278011ccc4
Walkoss <Walkoss@users.noreply.github.com>
2018-07-22 13:42:17 +0200

    Added absence command

--- esgi_bot/commands/absence.py
+++ esgi_bot/commands/absence.py
@@ -1,5 +1,6 @@
+import discord
 from discord.ext import commands
-from esgi_bot.users import is_authenticated
+from esgi_bot.users import is_authenticated, get_user
 
 
 class Absence:
@@ -8,8 +9,23 @@ class Absence:
 
     @commands.command(pass_context=True)
     @is_authenticated()
-    async def absences(self):
-        await self.bot.say('Getting all absences')
+    async def absences(self, ctx):
+        scraper = get_user(ctx.message.author.id)
+        if scraper:
+            absences = scraper.get_absences()
+            if absences:
+                embed = discord.Embed(colour=0x217bb1)
+                embed.set_footer(text="Scrapped from MyGES",
+                                 icon_url="https://www.myges.fr/assets/img/icons/favicon.png")
+                for absence in absences:
+                    embed.add_field(name="Date", value=absence["date"], inline=True)
+                    embed.add_field(name="Type", value=absence["type"], inline=True)
+                    embed.add_field(name="Justified", value=absence["justified"], inline=True)
+                    embed.add_field(name="Subject", value=absence["subject"])
+                    await self.bot.say(embed=embed)
+                    embed.clear_fields()
+            else:
+                await self.bot.say("You don't have absence :)")
 
 
 def setup(bot):
